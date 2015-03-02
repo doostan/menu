8a1c56f5b94ffa395ba7180d134bcfcdb6ec44da
doostan <doostan.github@gmail.com>
2015-03-02 19:29:33 +0330

    add a docblock

--- src/Doostan/Menu/Item.php
+++ src/Doostan/Menu/Item.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Doostan\Menu component.
+ * 
+ * @license http://opensource.org/licenses/MIT MIT license
+ * @copyright Copyright (c) 2015 https://github.com/doostan
+ * @author doostan doostan.github@gmail.com
+ */
 namespace Doostan\Menu;
 
 class Item implements \Iterator, \Countable
